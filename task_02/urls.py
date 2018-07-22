d3f60ef6fd18e5b36c029eaa50563839d7f2b201
Sindi <sjzalmutairi@gmail.com>
2018-07-22 19:25:51 +0300

    finished task

--- task_02/urls.py
+++ task_02/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from restaurants.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',,name='hello-world'),
+    path('home/', home, name='hello-world'),
 ]
